861ec051378f5be1870d57410bebc1f9b9b9df47
Tiago Funk <tiagoff.tf@gmail.com>
2018-05-22 21:36:08 -0300

    Tudo que eu fiz há mais no trabalho

--- Cuca/public_html/js/script.js
+++ Cuca/public_html/js/script.js
@@ -1,3 +1,4 @@
 // TODO: Coloque seu código JavaScript todo aqui!!!
 
 // você pode acessar RESULTS_MAP de constantes.js nesse arquivo, desde que ele tenha sido incluído antes do script.js no index.html
+
