7d2faba6fbe45dceeb9d0cc743a3d0c78bae57b8
Tiago Funk <tiagoff.tf@gmail.com>
2018-05-28 19:59:08 -0300

    Exclui umas pastas desencessárias

--- js/script.js
+++
@@ -1,3 +0,0 @@
-// TODO: Coloque seu código JavaScript todo aqui!!!
-
-// você pode acessar RESULTS_MAP de constantes.js nesse arquivo, desde que ele tenha sido incluído antes do script.js no index.html
